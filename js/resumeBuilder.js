595c4fa1513d78d4c804346b9f491aa78d16321f
Greg Palen <codingvirtual@gmail.com>
2015-03-26 06:47:02 -0500

    display project.projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -23,52 +23,58 @@
 var bio = {
     "name": "Greg Palen",
     "role": "Web Developer",
-    "contacts": {
-        "twitter": "@GPalen",
-        "github": "codingvirtual",
-        "location": "St Louis, MO",
-        "email": "codingvirtual@gmail.com"
+    "contacts": {   "twitter": "@GPalen",
+                    "github": "codingvirtual",
+                    "location": "St Louis, MO",
+                    "email": "codingvirtual@gmail.com"
     },
     "picUrl": "images/avatar-xhdpi.jpg",
     "welcomeMessage": "Hi and thank you for reviewing my interactive resume",
-    "skills": ["Java", "Android Applications","AngularJS", "mySQL", "JavaScript", "Google App Engine", "Google Cloud Endpoints","PHP", "Git"]
+    "skills": ["Java", "Android Applications", "AngularJS", "mySQL", "JavaScript", "Google App Engine", "Google Cloud Endpoints", "PHP", "Git"]
 };
 
-var projects = [{
-    "title": "Project 0 - Hello World",
-    "dates worked": "March 2015",
-    "description": "Class Hello World in HTML & CSS",
-    "images": ["url_goes_here"]
-}];
+var projects = {
+    "projects": [
+        {
+            "title": "Project 0 - Hello World",
+            "dates": "March 2015",
+            "description": "Class Hello World in HTML & CSS",
+            "images": ["url_goes_here"]
+        }
+    ]
+};
 
-var work = [{
-    "employer": "AT&T",
-    "title": "Application Sales Executive",
-    "datesWorked": "1998 - present",
-    "location": "St Louis",
-    "description": "Product Specialist supporting Unified Communications"
-}];
+var work = {
+    "jobs" : [
+        {   "employer": "AT&T",
+            "title": "Application Sales Executive",
+            "datesWorked": "1998 - present",
+            "location": "St Louis",
+            "description": "Product Specialist supporting Unified Communications"
+        }
+    ]
+};
 
 var education = {
-    "schools": [{
-        "name": "Univ. of Illinois - Springfield",
-        "years": 2015,
-        "city": "Springfield, MO (via Online Program)",
-        "majors": ["Computer Science"]
-    },
-        {
-            "name": "Univ. of Missouri - St Louis",
+    "schools": [
+        {   "name": "Univ. of Illinois - Springfield",
+            "years": 2015,
+            "city": "Springfield, MO (via Online Program)",
+            "majors": ["Computer Science"]
+        },
+        {   "name": "Univ. of Missouri - St Louis",
             "years": "2011 - 2013",
             "city": "St Louis, MO",
             "majors": ["Worked towards Computer Science"]
         }
     ],
-    "onlineCourses": [{
-        "title": "Intro to JavaScript",
-        "school": "Udacity.com",
-        "dates": "2015",
-        "url": "https://www.udacity.com/course/ud804"
-    }]
+    "onlineCourses": [
+        {   "title": "Intro to JavaScript",
+            "school": "Udacity.com",
+            "dates": "2015",
+            "url": "https://www.udacity.com/course/ud804"
+        }
+    ]
 };
 
 var token = "%data%"
@@ -77,10 +83,10 @@ $('#header').prepend(HTMLheaderName.replace(token, bio.name));
 $('#header').append(HTMLbioPic.replace(token, bio.picUrl));
 
 function showContactInfo() {
-        $('#topContacts')
-            .append(HTMLtwitter.replace(token, bio.contacts.twitter))
-            .append(HTMLemail.replace(token, bio.contacts.email))
-            .append(HTMLgithub.replace(token, bio.contacts.github));
+    $('#topContacts')
+        .append(HTMLtwitter.replace(token, bio.contacts.twitter))
+        .append(HTMLemail.replace(token, bio.contacts.email))
+        .append(HTMLgithub.replace(token, bio.contacts.github));
 }
 
 function showSkills() {
@@ -93,28 +99,32 @@ function showSkills() {
 }
 
 function displayWork() {
-    for (workEntry in work) {
+    for (workEntry in work.jobs) {
         $('#workExperience').append(HTMLworkStart);
-        var workEmployerTitle = HTMLworkEmployer.replace(token, work[workEntry].employer);
-        workEmployerTitle += HTMLworkTitle.replace(token, work[workEntry].title);
+        var workEmployerTitle = HTMLworkEmployer.replace(token, work.jobs[workEntry].employer);
+        workEmployerTitle += HTMLworkTitle.replace(token, work.jobs[workEntry].title);
         $('.work-entry:last').append(workEmployerTitle);
-        $('.work-entry:last').append(HTMLworkDates.replace(token, work[workEntry].datesWorked));
-        $('.work-entry:last').append(HTMLworkLocation.replace(token, work[workEntry].location));
-        $('.work-entry:last').append(HTMLworkDescription.replace(token, work[workEntry].description));
+        $('.work-entry:last').append(HTMLworkDates.replace(token, work.jobs[workEntry].datesWorked));
+        $('.work-entry:last').append(HTMLworkLocation.replace(token, work.jobs[workEntry].location));
+        $('.work-entry:last').append(HTMLworkDescription.replace(token, work.jobs[workEntry].description));
     }
 }
-
 function inName(name) {
-    console.log("name is " + name);
     var nameTokens = name.trim().split(" ");
     nameTokens[1] = nameTokens[1].toUpperCase();
     return nameTokens[0] + ' ' + nameTokens[1];
 }
 $('#main').append(internationalizeButton);
 
-projects.display = function() {
-    for (project in projects) {
-        $()
+projects.display = function () {
+    for (projectEntry in projects.projects) {
+        $('#projects').append(HTMLprojectStart);
+        $('.project-entry:last').append(HTMLprojectTitle.replace(token, projects.projects[projectEntry].title))
+        .append(HTMLprojectDates.replace(token, projects.projects[projectEntry].dates))
+        .append(HTMLprojectDescription.replace(token, projects.projects[projectEntry].description));
+        for (imageEntry in projects.projects[projectEntry].images) {
+            $('.project-entry:last').append(HTMLprojectImage.replace(token, projects.projects[projectEntry].images[imageEntry]));
+        }
     }
 }
 
@@ -122,3 +132,4 @@ projects.display = function() {
 showContactInfo();
 showSkills();
 displayWork();
+projects.display();
