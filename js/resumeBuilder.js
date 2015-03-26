1ccb91ccc945a3fc8a7738c22ad954bcc5632f3c
Greg Palen <codingvirtual@gmail.com>
2015-03-26 10:40:55 -0500

    added education and modified CSS to get the map to show the way I want it.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,20 +1,20 @@
 /*
 
- work contains an array of jobs. Each job object in jobs should contain an 
+ work contains an array of jobs. Each job object in jobs should contain an
  employer, title, location, dates worked and description.
 
- projects contains an array of projects. Each project object in projects 
- should contain a title, dates worked, description, and an images array with 
+ projects contains an array of projects. Each project object in projects
+ should contain a title, dates worked, description, and an images array with
  URL strings for project images.
 
- bio contains a name, role, welcomeMessage, contacts object and skills array. 
- The contacts object should contain (but doesn't have to) a mobile number, 
+ bio contains a name, role, welcomeMessage, contacts object and skills array.
+ The contacts object should contain (but doesn't have to) a mobile number,
  email address, github username, twitter handle and location.
 
- education contains an array of schools. Each school object in schools contains 
- a name, location, degree, majors array, dates attended and a url for the 
- school's website. education also contains an onlineCourses array. Each 
- onlineCourse object in onlineCourses should contain a title, school, dates 
+ education contains an array of schools. Each school object in schools contains
+ a name, location, degree, majors array, dates attended and a url for the
+ school's website. education also contains an onlineCourses array. Each
+ onlineCourse object in onlineCourses should contain a title, school, dates
  attended and a url for the course.
 
  */
@@ -23,10 +23,11 @@
 var bio = {
     "name": "Greg Palen",
     "role": "Web Developer",
-    "contacts": {   "twitter": "@GPalen",
-                    "github": "codingvirtual",
-                    "location": "St Louis, MO",
-                    "email": "codingvirtual@gmail.com"
+    "contacts": {
+        "twitter": "@GPalen",
+        "github": "codingvirtual",
+        "location": "St Louis, MO",
+        "email": "codingvirtual@gmail.com"
     },
     "picUrl": "images/avatar-xhdpi.jpg",
     "welcomeMessage": "Hi and thank you for reviewing my interactive resume",
@@ -48,8 +49,9 @@ var projects = {
 };
 
 var work = {
-    "jobs" : [
-        {   "employer": "AT&T",
+    "jobs": [
+        {
+            "employer": "AT&T",
             "title": "Application Sales Executive",
             "datesWorked": "1998 - present",
             "location": "St Louis",
@@ -60,19 +62,24 @@ var work = {
 
 var education = {
     "schools": [
-        {   "name": "Univ. of Illinois - Springfield",
-            "years": 2015,
+        {
+            "name": "Univ. of Illinois - Springfield",
+            "dates": "2015",
             "location": "Springfield, IL",
-            "majors": ["Computer Science"]
+            "majors": ["Computer Science"],
+            "degree": "Bachelor's Degree"
         },
-        {   "name": "Univ. of Missouri - St Louis",
-            "years": "2011 - 2013",
+        {
+            "name": "Univ. of Missouri - St Louis",
+            "dates": "2011 - 2013",
             "location": "St Louis, MO",
-            "majors": ["Worked towards Computer Science"]
+            "majors": ["Computer Science"],
+            "degree": "Progress towards Bachelor's Degree"
         }
     ],
     "onlineCourses": [
-        {   "title": "Intro to JavaScript",
+        {
+            "title": "Intro to JavaScript",
             "school": "Udacity.com",
             "dates": "2015",
             "url": "https://www.udacity.com/course/ud804"
@@ -82,10 +89,11 @@ var education = {
 
 var token = "%data%"
 
-bio.display = function() {
-    $('#header').prepend(HTMLheaderRole.replace(token, bio.role));
-    $('#header').prepend(HTMLheaderName.replace(token, bio.name));
-    $('#header').append(HTMLbioPic.replace(token, bio.picUrl));
+bio.display = function () {
+    $('#header')
+        .prepend(HTMLheaderRole.replace(token, bio.role))
+        .prepend(HTMLheaderName.replace(token, bio.name))
+        .append(HTMLbioPic.replace(token, bio.picUrl));
     bio.contacts.display();
     bio.skills.display();
 }
@@ -97,7 +105,7 @@ bio.contacts.display = function () {
         .append(HTMLgithub.replace(token, bio.contacts.github));
 }
 
-bio.skills.display = function() {
+bio.skills.display = function () {
     if (bio.skills.skills.length > 0) {
         $('#header').append(HTMLskillsStart);
         for (var skill in bio.skills.skills) {
@@ -106,30 +114,57 @@ bio.skills.display = function() {
     }
 }
 
-work.display = function() {
-    for (var workEntry in work.jobs) {
-        $('#workExperience').append(HTMLworkStart);
-        var workEmployerTitle = HTMLworkEmployer.replace(token, work.jobs[workEntry].employer);
-        workEmployerTitle += HTMLworkTitle.replace(token, work.jobs[workEntry].title);
-        $('.work-entry:last').append(workEmployerTitle);
-        $('.work-entry:last').append(HTMLworkDates.replace(token, work.jobs[workEntry].datesWorked));
-        $('.work-entry:last').append(HTMLworkLocation.replace(token, work.jobs[workEntry].location));
-        $('.work-entry:last').append(HTMLworkDescription.replace(token, work.jobs[workEntry].description));
-    }
-}
-
-projects.display = function () {
-    for (var projectEntry in projects.projects) {
-        $('#projects-bundle').append(HTMLprojectStart);
-        $('.project-entry').append(HTMLprojectTitle.replace(token, projects.projects[projectEntry].title))
-        .append(HTMLprojectDates.replace(token, projects.projects[projectEntry].dates))
-        .append(HTMLprojectDescription.replace(token, projects.projects[projectEntry].description));
-        for (var imageEntry in projects.projects[projectEntry].images) {
-            $('.project-images').append(HTMLprojectImage.replace(token, projects.projects[projectEntry].images[imageEntry]));
+work.display = function () {
+    if (work.jobs.length > 0) {
+        for (var workEntry in work.jobs) {
+            $('#workExperience').append(HTMLworkStart);
+            var workEmployerTitle = HTMLworkEmployer.replace(token, work.jobs[workEntry].employer);
+            workEmployerTitle += HTMLworkTitle.replace(token, work.jobs[workEntry].title);
+            $('.work-entry')
+                .append(workEmployerTitle)
+                .append(HTMLworkDates.replace(token, work.jobs[workEntry].datesWorked))
+                .append(HTMLworkLocation.replace(token, work.jobs[workEntry].location))
+                .append(HTMLworkDescription.replace(token, work.jobs[workEntry].description));
         }
     }
 }
 
+projects.display = function () {
+    if (projects.projects.length > 0) {
+        for (var projectEntry in projects.projects) {
+            $('#projects-bundle').append(HTMLprojectStart);
+            $('.project-entry').append(HTMLprojectTitle.replace(token, projects.projects[projectEntry].title))
+                .append(HTMLprojectDates.replace(token, projects.projects[projectEntry].dates))
+                .append(HTMLprojectDescription.replace(token, projects.projects[projectEntry].description));
+            if (projects.projects[projectEntry].images.length > 0) {
+                for (var imageEntry in projects.projects[projectEntry].images) {
+                    $('.project-images').append(HTMLprojectImage.replace(token, projects.projects[projectEntry].images[imageEntry]));
+                }
+            }
+        }
+    }
+}
+
+education.display = function () {
+    if (education.schools.length > 0) {
+        for (var school in education.schools) {
+            $('#education').append(HTMLschoolStart);
+            $('.education-entry')
+                .append(HTMLschoolName.replace(token, education.schools[school].name))
+                .append(HTMLschoolDegree.replace(token, education.schools[school].degree))
+                .append(HTMLschoolDates.replace(token, education.schools[school].dates))
+                .append(HTMLschoolLocation.replace(token, education.schools[school].location));
+            if (education.schools[school].majors.length > 0) {
+                for (var major in education.schools[school].majors) {
+                    $('.education-entry').append(HTMLschoolMajor.replace(token, education.schools[school].majors[major]));
+                }
+            }
+        }
+    }
+    if (education.onlineCourses.length > 0) {
+        console.log("online");
+    }
+}
 
 function inName(name) {
     var nameTokens = name.trim().split(" ");
@@ -141,4 +176,5 @@ $('#main').append(internationalizeButton);
 bio.display();
 work.display();
 projects.display();
+education.display();
 $('#mapDiv').append(googleMap);
